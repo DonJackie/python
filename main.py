906548c1d0d43a7ba5acf18ca2b9d3b8030696aa
donghyunlim <o12@kakao.com>
2022-12-20 01:06:00 +0900

    Diffrence between 'from' and 'import'

--- main.py
+++ main.py
@@ -2,7 +2,13 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from data import contacts
 
+jenny_email = contacts["Jenny"]["email"]
+
+def my_function(n1,n2):
+    total = n1 + n2
+    return total
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +17,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    print(my_function(1,2))
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
